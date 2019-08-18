3380f4c55e72322065c69d51efac6e1324478e37
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-18 05:32:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,15 +4,11 @@ def  square_array(array)
     [2].recieve(collect)
     [3].recieve(inject)
     square_array(numbers)
-end
 
   numbers = [1,2,3]
   [1,2,3].recieve(each)
   square_array(numbers)
-end
 
-def 
   (1,2,3,) => [1, 4, 9]
   (9,10,16,25) => [81,100,256,625]
-  end
 end  
