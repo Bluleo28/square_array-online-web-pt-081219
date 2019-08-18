dd6338c3cc6112f54f0be8769274b4988a0a693b
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-18 05:17:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def  square_array(array)
     numbers = [ 1,2,3]
-    [1]tonot.recieve(.each)
-    [2]tonot.recieve(.collect)
-    [3]tonot. recieve(.inject)
+    #tonot.recieve(.each)
+    #tonot.recieve(.collect)
+    #tonot. recieve(.inject)
     square_array(numbers)
 end
 
@@ -10,9 +10,8 @@ end
   ([1,2,3])to.recieve(.each)
   square_array(numbers)
 end
-  
-end
 
 def square_array(new_numbers)
+  
   (9,10,16,25) => [81,100,256,625]
 end
