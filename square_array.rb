de843343584cac244aeb3e0c03c1740d0b8db5f4
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-18 05:16:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,11 +2,15 @@ def  square_array(array)
     numbers = [ 1,2,3]
     [1]tonot.recieve(.each)
     [2]tonot.recieve(.collect)
-    [3]tonot. recieve(.inject)def square_array(numbers)
+    [3]tonot. recieve(.inject)
+    square_array(numbers)
 end
 
   numbers= [1,2,3]
   ([1,2,3])to.recieve(.each)
+  square_array(numbers)
+end
+  
 end
 
 def square_array(new_numbers)
